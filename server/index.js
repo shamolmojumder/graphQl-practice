2dc67c147a6cbca4ada2d810e6116f74cd4e8abd
shamolmojumder <shamolmojumder14@gmail.com>
2024-03-11 01:52:31 +0600

    23:00

--- server/index.js
+++ server/index.js
@@ -1,34 +1,57 @@
 const express = require('express');
-const {ApolloServer} = require('@apollo/server');
-const {expressMiddleware} = require('@apollo/server/express4');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
-const cors=require("cors")
+const cors = require("cors");
+const axios = require('axios');
 
-async function startServer(){
-    const app=express();
-    const server=new ApolloServer({
-        typeDefs:`
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+        type User {
+            id:ID!
+            name:String!
+            username:String!
+            email:String!
+            phone:String!
+            website:String!
+        }
             type Todo{
                 id:ID!
                 title:String!
                 completed:Boolean
+                userId:ID!
             }
             
             type Query{
                 getTodos:[Todo]
+                getAllUsers:[User]
+                getUser(id:ID!):User
             }
         `,
-        resolvers:{
-
-        }
+        resolvers: {
+            Query: {
+                getTodos: async () => (
+                    await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
+                getAllUsers: async () =>
+                    (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+                    getUser: async (parent,{id}) =>
+                    (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
+            },
+        },
     });
 
     app.use(bodyParser.json());
     app.use(cors());
-
     await server.start()
-    app.use("/graphql",expressMiddleware(server))
-    app.listen(8000,()=>console.log("server started for graphql"))
+    app.get('/', (req, res) => {
+        res.send('HI from GraphQl Project')
+      })
+      
+    app.use("/graphql", expressMiddleware(server))
+    app.listen(8000, () => console.log("server started for graphql"))
 }
 
 startServer()
